ebac3e3c8eca5372ed5604222763d439e47d7b20
JulesThuillier <thuillierjules@gmail.com>
2015-11-28 15:07:41 -0500

    small changes

--- Server/server/app.js
+++ Server/server/app.js
@@ -43,8 +43,8 @@ app.use(function(req,res,next){
     next();
 });
 
-app.use(cors());
-/*
+//app.use(cors());
+
 app.use(function (req, res, next){
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
@@ -52,7 +52,7 @@ app.use(function (req, res, next){
     res.header('Access-Control-Allow-Credentials', 'true');
 
     next();
-})*/
+})
 
 
 // We are going to protect /api routes with JWT
