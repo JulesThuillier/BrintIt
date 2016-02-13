bcc0278f9fdaef3eea26497fbca32957d4622e7e
julesthuillier <thuillierjules@gmail.com>
2016-02-13 15:23:00 -0500

    test for inf3995

--- Server/server/routes/events.js
+++ Server/server/routes/events.js
@@ -49,10 +49,10 @@ function createShadowUser(req, firstName, lastName, phone) {
         }, function(err, result) {
           if (err) {
               console.log("There was an error while creating a shadow user. Code : 2");
-            return;
+              return;
           }
             // Return the created user
-            console.log("User created");
+            console.log("User created ");
             return result;
         });
 }
